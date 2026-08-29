ec209db35bbec5cbe482ecc776771d0c84e1d42f
lods <carlobaron430@gmail.com>
2026-08-29 14:42:42 +0800

    hotfix: removed WasActive and move the methods to private

--- lib/CollisionHandler.hpp
+++ lib/CollisionHandler.hpp
@@ -12,15 +12,14 @@ using namespace std;
 class CollisionSystem{
 	private:
 		set<pair<ICollider*, ICollider*>> activeCollisions;
+		void AddActiveColliders(ICollider* colliderA, ICollider* colliderB);
+		int RemoveActiveColliders(ICollider* colliderA, ICollider* colliderB);
+		void ResolveCollision(ICollider* mover, ICollider* colliderA, ICollider* colliderB, optional<Vector2> mtv);
 
 	public:
-	void CollisionHandler(vector<GameObject*> gameObject);
-	optional<Vector2> CircleCircleCollision(CircleCollider* circleA, CircleCollider* circleB);
-	optional<Vector2> CircleRectangleCollision(CircleCollider* circle, RectangleCollider* rect);
-	optional<Vector2> RectangleRectangleCollision(RectangleCollider* rectA, RectangleCollider* rectB);
+		void CollisionHandler(vector<GameObject*> gameObject);
+		optional<Vector2> CircleCircleCollision(CircleCollider* circleA, CircleCollider* circleB);
+		optional<Vector2> CircleRectangleCollision(CircleCollider* circle, RectangleCollider* rect);
+		optional<Vector2> RectangleRectangleCollision(RectangleCollider* rectA, RectangleCollider* rectB);
 
-	bool WasActive(ICollider* colliderA, ICollider* colliderB);
-	void AddActiveColliders(ICollider* colliderA, ICollider* colliderB);
-	int RemoveActiveColliders(ICollider* colliderA, ICollider* colliderB);
-	void ResolveCollision(ICollider* mover, ICollider* colliderA, ICollider* colliderB, optional<Vector2> mtv);
 };
